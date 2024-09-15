97cac3d380cad3219adff211b538bd7aecc6e983
AnDev02 <135408714+AnDev02@users.noreply.github.com>
2024-09-16 00:02:50 +0700

    update

--- vision-led-be/src/Services/UserServices.js
+++ vision-led-be/src/Services/UserServices.js
@@ -103,7 +103,7 @@ const loginWithGoogle = (res, userLogin) => {
                     secure: true,
                     maxAge: 24 * 60 * 60 * 1000, 
                     sameSite: "strict"
-                }).json(rest);
+                }).json({ redirectTo: `${process.env.URL_CLIENT}/login-success/google/${emailId}` });
             } else {
                 const generatedPassword = 
                   Math.random().toString(36).slice(-8) + 
@@ -129,7 +129,7 @@ const loginWithGoogle = (res, userLogin) => {
                     secure: true,
                     maxAge: 24 * 60 * 60 * 1000, 
                     sameSite: "strict"
-                }).json(rest);
+                }).json({ redirectTo: `${process.env.URL_CLIENT}/login-success/google/${emailId}` });
                 //     res.redirect(`${process.env.URL_CLIENT}/login-success/${req.user?.provider}/${req.user?.id}`)
             }
         } catch (error) {
