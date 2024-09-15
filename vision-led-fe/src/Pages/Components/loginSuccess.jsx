08c8307e3b2ffc2e6b86cdd692c71c9b10d7dce9
AnDev02 <135408714+AnDev02@users.noreply.github.com>
2024-09-15 23:41:09 +0700

    update login ft

--- vision-led-fe/src/Pages/Components/loginSuccess.jsx
+++ vision-led-fe/src/Pages/Components/loginSuccess.jsx
@@ -1,53 +1,25 @@
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
-import * as UserServices from '../../Services/UserServices'
-import { useQuery } from '@tanstack/react-query'
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import jwt_decode from "jwt-decode";
-import { updateUser } from '../../Redux/Slides/userSlide';
-
+import { Navigate } from 'react-router-dom';
 
 export default function LoginSuccess() {
-    const userSelector = useSelector(state => state.user);
-    const orderSelector = useSelector(state => state.order);
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
-    const { provider, userId } = useParams()
-
-    const loginSuccess = async () => {
-        const res = await UserServices.LoginSuccess(provider, userId);
-        return res;
-    }
-
     const { isLoading, data, isSuccess } = useQuery(['userLoggedIn', provider, userId], loginSuccess);
 
     useEffect(() => {
-        if (isSuccess) {
-            if (data?.access_token !== undefined) {
-                localStorage.setItem('access_token', data?.access_token);
-                if (data?.access_token) {
-                    const decoded = jwt_decode(data?.access_token);
-                    if (decoded?.id) {
-                        handleGetDetailsUser(decoded?.id, data?.access_token);
-                    }
-                }
+        if (isSuccess && data?.access_token) {
+            localStorage.setItem('access_token', data?.access_token);
+            const decoded = jwt_decode(data?.access_token);
+            if (decoded?.id) {
+                handleGetDetailsUser(decoded?.id, data?.access_token);
             }
         }
-    }, [isSuccess])
-    const handleGetDetailsUser = async (id, token) => {
-        const res = await UserServices.GetDetailsUser(id, token);
-        dispatch(updateUser({ ...res?.data, access_token: token }))
+    }, [isSuccess]);
+
+    if (userSelector.inOrder === false && (isLoggedIn || isSuccess)) {
+        return <Navigate to='/' replace={true} />;
     }
-    const { isLoggedIn } = useSelector(state => state.user)
 
-    return (
-        <>
-            {userSelector.inOrder === false ?
-                (isLoggedIn || isSuccess) && <Navigate to={'/'} replace={true} />
-                :
-                (isLoggedIn || isSuccess) && <Navigate to={`/payment/form/${orderSelector.orderId}`} replace={true} />
+    if (userSelector.inOrder && (isLoggedIn || isSuccess)) {
+        return <Navigate to={`/payment/form/${orderSelector.orderId}`} replace={true} />;
+    }
 
-            }
-        </>
-    )
+    return <div>Loading...</div>;
 }
